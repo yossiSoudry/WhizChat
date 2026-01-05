b86086c39bd9c3e635a8dd21644d7c9091b86574
yossiSoudry <yosisudri@gmail.com>
2026-01-05 14:16:15 +0200

    0.31

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -5,16 +5,23 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area";
 
 import { cn } from "@/lib/utils";
 
+interface ScrollAreaProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> {
+  onScroll?: (event: React.UIEvent<HTMLDivElement>) => void;
+}
+
 const ScrollArea = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  HTMLDivElement,
+  ScrollAreaProps
+>(({ className, children, onScroll, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
-    ref={ref}
     className={cn("relative overflow-hidden", className)}
     {...props}
   >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+    <ScrollAreaPrimitive.Viewport
+      ref={ref}
+      className="h-full w-full rounded-[inherit]"
+      onScroll={onScroll}
+    >
       {children}
     </ScrollAreaPrimitive.Viewport>
     <ScrollBar />
